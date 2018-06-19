55c00fde815e9a3a1bd8dfdcc85171c2b5e21da1
Myakkatt <myao_03@icloud.com>
2018-06-19 20:34:07 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,20 @@
+
+def fizzbuzz(number)
+if  number%15 == 0
+  puts "FizzBuzz"
+  "FizzBuzz"
+  
+  elsif number%5 == 0
+  puts "Buzz"
+  "Buzz"
+  
+  elsif number%3 == 0
+  puts "Fizz"
+  "Fizz"
+  
+else
+  
+end
+end
+
+fizzbuzz(50)
