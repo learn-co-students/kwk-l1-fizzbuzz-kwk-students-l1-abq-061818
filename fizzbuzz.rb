b889a6780eb2f80c53f1633160bfb63eed52d06f
Krysta05 <martinezkrysta7@gmail.com>
2018-06-19 20:36:52 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(number)
+if number%15 == 0
+ puts "FizzBuzz"
+ "FizzBuzz"
+elsif number%5 == 0
+ puts "Buzz"
+ "Buzz"
+elsif number%3 == 0
+ puts "fizz"
+ "Fizz"
+else
+  puts "I GOT HERE YAYYY!!"
+end 
+end
+ fizzbuzz(14)
